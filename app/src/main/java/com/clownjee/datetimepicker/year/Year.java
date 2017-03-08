8df07572a9112e06433e61f22631aec00360de7d
Clownjee <megzzi@163.com>
2017-03-08 14:07:32 +0800

    beta版

---
+++ app/src/main/java/com/clownjee/datetimepicker/year/Year.java
@@ -0,0 +1,27 @@
+package com.clownjee.datetimepicker.year;
+
+/**
+ * Created by Clownjee on 2017/1/26.
+ */
+public class Year {
+
+    private int index;
+    private String tag;
+
+    public Year(int index, String tag) {
+        this.index = index;
+        this.tag = tag;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getTag() {
+        return this.tag;
+    }
+}
